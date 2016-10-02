63575f5c17c2776b360b3c500cd8a57636a67ddd
Oskar Ehnstrom <oskar.ehnstrom@futurice.com>
2016-10-02 12:59:59 +0200

    Add models

--- admin.py
+++ admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Entry, Item, LineItem
 
-# Register your models here.
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+	date_hierarchy = 'timestamp'
+	list_display = ('timestamp', 'name')
+	list_filter = ('archived',)
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+	list_display = ('name', 'description')
+
+@admin.register(LineItem)
+class LineItemAdmin(admin.ModelAdmin):
+	list_display = ('entry', 'item', 'amount')
+	readonly_fields = ('entry',)
--- models.py
+++ models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+	""" An item that a user can put on their tab """
+	name = models.CharField(max_length=200)
+	description = models.TextField(blank=True)
+
+	def __str__(self):
+		return self.name
+
+class Entry(models.Model):
+	""" An entry for a user adding someting to their tab """
+	name = models.CharField(max_length=200)
+	timestamp  = models.DateTimeField(auto_now_add=True)
+	archived = models.BooleanField(default=False)
+
+	def datetime(self):
+		return self.timestamp.strftime('%Y-%m-%d %H:%M')
+
+	def __str__(self):
+		return '%s, %s'%(self.datetime(), self.name)
+
+class LineItem(models.Model):
+	""" Line item to enable one entry to have multiple items """
+	amount = models.PositiveSmallIntegerField()
+	item = models.ForeignKey(Item, on_delete=models.PROTECT)
+	entry = models.ForeignKey(Entry, on_delete=models.CASCADE, editable=False)
+
+	def __str__(self):
+		return '%s x%d'%(self.item.name, self.amount)

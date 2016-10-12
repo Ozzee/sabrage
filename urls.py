e5817d2302f90d59a07844b606a2006793f4d805
Oskar Ehnstrom <oskar.ehnstrom@futurice.com>
2016-10-12 21:23:01 +0200

    Start work on UI

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^entries/$', views.entries, name='entries')
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
+    url(r'^entries/$', views.entries, name='entries'),
+    url(r'^users/$', views.suggest_users)
 ]

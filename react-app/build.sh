00639002e950f45467064eb0fe6a05fc2bef351b
Oskar Ehnstrom <oskar.ehnstrom@futurice.com>
2017-07-20 21:10:07 +0200

    Change build script a little

--- react-app/build.sh
+++ react-app/build.sh
@@ -5,8 +5,6 @@ node node_modules/react-scripts/bin/react-scripts.js build
 
 find build/static/js -type f -exec sed -i '.bak' 's/http:\/\/localhost:8000//g' {} \;
 
-rm build/statis/js/*.bak
-
 cp build/index.html ../templates/
 
 rm -r ../static/sabrage/static/*
